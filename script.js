ba243004d5d2d77be434f9909f8d878abec9b7d4
Sindu Andita Pratama <sindu.andita@gmail.com>
2024-09-03 11:35:04 +0700

    Rewrite calculator logic

--- script.js
+++ script.js
@@ -1,85 +1,166 @@
 // DOM Bindings
 const calculator = document.querySelector('.calc');
-const display = document.querySelector('.calc__display');
+const calculatorDisplay = document.querySelector('.calc__display');
 
-let number1 = '0';
-let number2 = '0';
-let operand;
+let number1 = null;
+let number2 = null;
+let operand = null;
+let result = null;
 
-function processNumber(input) {
+function handleNumber(input) {
+  if (result) result = null;
   if (!operand) {
-    if (!number1 && input === '.') number1 = '0';
-    number1 = String(+number1 || '').concat(input);
-    printDisplay(number1);
-  } else if (operand) {
-    if (!number2 && input === '.') number2 = '0';
-    number2 = String(+number2 || '').concat(input);
-    printDisplay(number2);
+    if (!number1) {
+      number1 = Number(input);
+    } else {
+      if (Number(input) === 0) {
+        number1 = String(number1) + input;
+      } else {
+        number1 = Number(String(number1) + input);
+      }
+    }
+    display(number1);
+  } else {
+    if (!number2) {
+      number2 = Number(input);
+    } else {
+      if (Number(input) === 0) {
+        number2 = String(number2) + input;
+      } else {
+        number2 = Number(String(number2) + input);
+      }
+    }
+    display(number2);
   }
 }
 
-function processCalculate(n1, n2, op) {
-  if (op === '+') {
-    number1 = Number(n1) + Number(n2) + '';
-  } else if (op === '-') {
-    number1 = Number(n1) - Number(n2) + '';
-  } else if (op === '/') {
-    number1 = Number(n1) / Number(n2) + '';
-  } else if (op === 'x') {
-    number1 = Number(n1) * Number(n2) + '';
+function handleSign() {
+  if (result) return;
+  if (!operand) {
+    number1 = -number1;
+    display(number1);
+  } else {
+    number2 = -number2;
+    display(number2);
   }
-  number2 = '';
-  operand = '';
-  printDisplay(number1);
 }
 
-function printDisplay(content) {
-  content = content + '';
-  let decimals;
-  if (content.includes('.')) {
-    [content, decimals] = content.split('.');
-    decimals = decimals.slice(0, 2);
-    content += '.' + decimals;
+function handleOperator(symbol) {
+  if (
+    (symbol === operand && !number2) ||
+    (!operand && !number1 && !number2 && !result)
+  ) {
+    return;
   }
-  display.textContent = content;
+
+  if (number1 && number2 && operand) {
+    calculate();
+    operand = symbol;
+  }
+  if (number1 && number2 && result) {
+    number1 = result;
+    operand = symbol;
+    result = null;
+  }
+  if (!number2 && !operand) {
+    operand = symbol;
+  }
+  if (result) {
+    number1 = result;
+    operand = symbol;
+    result = null;
+  }
+  operand = symbol;
+}
+
+function calculate() {
+  if (number1 === null || number2 === null) return;
+
+  if (operand === '+') {
+    result = (number1.toFixed(2) * 100 + number2.toFixed(2) * 100) / 100;
+  } else if (operand === '-') {
+    result = (number1.toFixed(2) * 100 - number2.toFixed(2) * 100) / 100;
+  } else if (operand === '/') {
+    result = number1 / number2;
+    result = (result.toFixed(2) * 100) / 100;
+  } else if (operand === 'x') {
+    result = number1 * number2;
+    result = (result.toFixed(2) * 100) / 100;
+  }
+  number1 = number2 = null;
+  operand = null;
+  display(result);
+}
+
+function handleAC() {
+  number1 = null;
+  number2 = null;
+  operand = null;
+  result = null;
+  display(0);
+}
+
+function handleCE() {
+  if (!operand) number1 = null;
+  else number2 = null;
+  display(0);
+}
+
+function handleDEL() {
+  let strNumber;
+  if (result) return;
+  if (!operand) {
+    strNumber = String(number1);
+    number1 = Number(strNumber?.slice(0, -1)) ?? 0;
+    display(number1);
+  } else {
+    strNumber = String(number2);
+    number2 = Number(strNumber?.slice(0, -1)) ?? 0;
+    display(number2);
+  }
+}
+
+function handleDecimal() {
+  if (!operand) {
+    number1 = null;
+    if (Number.isInteger(number1) || number1 === null) {
+      if (number1) {
+        number1 = number1 + '.';
+      } else {
+        number1 = '0.';
+        display(0);
+      }
+    }
+  } else {
+    if (Number.isInteger(number2) || number2 === null) {
+      number2 = null;
+      if (number2) {
+        number2 = number2 + '.';
+      } else {
+        number2 = '0.';
+        display(0);
+      }
+    }
+  }
+}
+
+function display(content) {
+  if (Number.isNaN(content)) content = 0;
+  calculatorDisplay.textContent = content;
 }
 
 function handleClick(e) {
   if (!e.target instanceof HTMLButtonElement) return;
   const symbol = e.target.textContent;
-  if (!Number.isNaN(Number(symbol)) || symbol === '.') processNumber(symbol);
-  if ('x/+-'.includes(symbol)) {
-    if (operand) processCalculate(number1, number2, operand);
-    operand = symbol;
-  }
-  if (symbol === '=') processCalculate(number1, number2, operand);
-  if (symbol === 'AC') {
-    number1 = number2 = operand = '';
-    printDisplay('0');
-  }
-  if (symbol === 'CE') {
-    if (!operand) number1 = '';
-    else number2 = '';
-    printDisplay('0');
-  }
-  if (symbol === 'DEL') {
-    if (!operand) {
-      number1 = number1?.slice(0, -1) || '0';
-      printDisplay(number1);
-    } else {
-      number2 = number2?.slice(0, -1) || '0';
-      printDisplay(number2);
-    }
-  }
-  if (symbol === '+/-') {
-    if (!operand) {
-      number1 = number1.startsWith('-') ? number1.slice(1) : `-${number1}`;
-      printDisplay(number1);
-    } else {
-      number2 = number2.startsWith('-') ? number2.slice(1) : `-${number2}`;
-      printDisplay(number2);
-    }
-  }
+  if (!Number.isNaN(Number(symbol))) handleNumber(symbol);
+  if ('x/+-'.includes(symbol)) handleOperator(symbol);
+  if (symbol === 'AC') handleAC();
+  if (symbol === 'CE') handleCE();
+  if (symbol === 'DEL') handleDEL();
+  if (symbol === '+/-') handleSign();
+  if (symbol === '=' && operand) calculate();
+  if (symbol === '.') handleDecimal();
+  console.log({ number1, number2, operand, result });
 }
 
 calculator.addEventListener('click', handleClick);
